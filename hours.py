b6a1379211bbc56d3d6dc0a8e48115788d5798d4
Samuel <sam.o.bryson@gmail.com>
2018-12-04 15:27:02 +0100

    I think I have got it this time

--- hours.py
+++ hours.py
@@ -183,12 +183,10 @@ bot.enable_save_next_step_handlers(delay=1)
 # # WARNING It will work only if enable_save_next_step_handlers was called!
 bot.load_next_step_handlers()
 
-bot.infinity_polling(True)
 bot.set_update_listener(listener)
 
-
 def main_loop():
-    bot.polling(True)
+    bot.infinity_polling(True)
     while True:
         time.sleep(3)
 
